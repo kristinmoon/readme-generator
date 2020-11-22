6e10ea23f916bfef89651d02cb47bd929d499a6f
kristinmoon <71245424+kristinmoon@users.noreply.github.com>
2020-11-22 11:43:31 -0700

    add inquirer

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
+console.log(inquirer)
 
 // array of questions for user
 const questions = [
